0cfef078bd2e5f1919ec62da1eca09b3ef46b0b3
wildnhl <minnesotanhlwild@gmail.com>
2023-12-07 17:43:07 +0300

    refactor(trello): some refactor code

--- src/js/template.js
+++ src/js/template.js
@@ -14,9 +14,9 @@ function buildTemplateCard({
     noneTitle = '',
     nonePerform = '';
 
-  noneDesc = description === '' ? 'style = "display: none"' : '';
-  noneTitle = title === '' ? 'style = "display: none"' : '';
-  nonePerform = performer === '' ? 'style = "display: none"' : '';
+  noneDesc = description === '' ? 'd-none' : '';
+  noneTitle = title === '' ? 'd-none' : '';
+  nonePerform = performer === '' ? 'd-none' : '';
 
   switch (place) {
     case 'todo':
@@ -35,14 +35,14 @@ function buildTemplateCard({
 
   return `
     <div class="card ${background} d-flex gap-2 flex-column p-1" id="${id}">
-      <h3 class="card__title" ${noneTitle}>${title}</h3>
-      <p class="card__description" ${noneDesc}>${description}</p>
-      <div class="d-flex gap-2">
-      <select class="form-select" name="fromto" data-id="select-place">
-      <option value="todo" ${selectTodo}>Todo</option>
-      <option value="inprogress" ${selectInProgress}>In progress</option>
-      <option value="done" ${selectDone}>Done</option>
-      </select>
+      <h3 class="card__title ${noneTitle}" >${title}</h3>
+      <p class="card__description ${noneDesc}">${description}</p>
+      <div class="d-flex gap-2 cardBtns">
+        <select class="form-select" name="fromto" data-id="select-place">
+          <option value="todo" ${selectTodo}>Todo</option>
+          <option value="inprogress" ${selectInProgress}>In progress</option>
+          <option value="done" ${selectDone}>Done</option>
+        </select>
         <button
           class="btn btn-primary btn-sm"
           type="button"
@@ -52,11 +52,17 @@ function buildTemplateCard({
         >
           Edit
         </button>
-        <button class="btn btn-danger btn-sm" data-id="btn-delete" data-bs-toggle="modal"
-              data-bs-target="#deleteSpecModal">Delete</button>
+        <button
+          class="btn btn-danger btn-sm"
+          data-id="btn-delete"
+          data-bs-toggle="modal"
+          data-bs-target="#deleteSpecModal"
+        >
+          Delete
+        </button>
       </div>
-      <div class="d-flex justify-content-between gap-2">
-        <h3 class="card-date" ${nonePerform}>Person: ${performer}</h3>
+      <div class="cardBottom d-flex justify-content-between gap-2">
+        <h3 class="card-date ${nonePerform}">Person: ${performer}</h3>
         <h3 class="card-date ms-auto">${createdAt}</h3>
       </div>
     </div>
